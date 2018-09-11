45fb2ccfbf5c92316dcf4a2327997f2a584b8a1e
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-11 14:18:46 +0530

    RNATranscription-Solution-@hellokuldeep-1.1

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace RNATranscriptionModule
-{
-    public static class RnaTranscription
-    {
-        public static string ToRna(string nucleotide)
-        {
-            throw new NotImplementedException("You need to implement this function.");
+namespace RNATranscriptionModule{
+    public static class RnaTranscription{
+        public static string ToRna(string nucleotide){
+            int n = nucleotide.Length;
+            string nucleotide2 = null;
+            for(int i = 0; i < n; i++){
+                switch (nucleotide[i]){
+                    case 'G':
+                        nucleotide2 += 'C';
+                        break;
+                    case 'C':
+                        nucleotide2 += 'G';
+                        break;
+                    case 'T':
+                        nucleotide2 += 'A';
+                        break;
+                    case 'A':
+                        nucleotide2 += 'U';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return nucleotide2;
         }
     }
 }
