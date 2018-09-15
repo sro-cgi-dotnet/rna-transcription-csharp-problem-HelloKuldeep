618395279bff52b013cfb80d45775099d9a0e144
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-15 15:39:28 +0530

    Solution-RNA-@hellokuldeep-2.1

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,28 +1,29 @@
 ﻿using System;
+using System.Text;
 namespace RNATranscriptionModule{
     public static class RnaTranscription{
-        public static string ToRna(string nucleotide){
+        public static string ToRna(String nucleotide){
             int n = nucleotide.Length;
-            string nucleotide2 = null;
+            StringBuilder nucleotide2 = new StringBuilder("",50); //To avoid immutable property of String
             for(int i = 0; i < n; i++){
                 switch (nucleotide[i]){
                     case 'G':
-                        nucleotide2 += 'C';
+                        nucleotide2.Append("C");
                         break;
                     case 'C':
-                        nucleotide2 += 'G';
+                        nucleotide2.Append("G");
                         break;
                     case 'T':
-                        nucleotide2 += 'A';
+                        nucleotide2.Append("A");
                         break;
                     case 'A':
-                        nucleotide2 += 'U';
+                        nucleotide2.Append("U");
                         break;
                     default:
                         break;
                 }
             }
-            return nucleotide2;
+            return nucleotide2.ToString();
         }
     }
 }
